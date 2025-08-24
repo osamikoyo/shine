69c853560f183c0dda13e4dcd8a2218a65d408f9
osamikoyo <v.pylayev@mail.ru>
2025-08-25 04:52:35 +0500

    update main.go

--- main.go
+++ main.go
@@ -8,28 +8,48 @@ import (
 )
 
 func main() {
-	reader := bufio.NewReader(os.Stdin)
 	repl := NewRepl()
 
+	if len(os.Args) > 2 && os.Args[1] == "-f" {
+		filename := os.Args[2]
+		code, err := os.ReadFile(filename)
+		if err != nil {
+			fmt.Printf("Error reading file %s: %v\n", filename, err)
+			os.Exit(1)
+		}
+		fmt.Println("Input from file:", string(code))
+		err = repl.StartRepl(string(code))
+		if err != nil {
+			fmt.Printf("Error: %v\n", err)
+			os.Exit(1)
+		}
+		return
+	}
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Welcome to Shine Lisp REPL! Type 'exit' to quit.")
+
 	for {
-		fmt.Print("shine~~>")
+		fmt.Print("shine~~> ")
 		code, err := reader.ReadString('\n')
 		if err != nil {
-			fmt.Println(err)
-
-			return
-		}
-
-		if code == "" {
-			continue
-		}
-		if strings.HasPrefix(code, "exit") {
-			fmt.Println("Shine on your crazy diamond")
+			fmt.Printf("Error reading input: %v\n", err)
 			break
 		}
 
-		if err := repl.StartRepl(code); err != nil {
-			fmt.Println(err)
+		code = strings.TrimSpace(code)
+		if code == "" {
+			continue
+		}
+		if code == "exit" {
+			fmt.Println("Shine on Your Crazy Diamond!")
+			break
+		}
+
+		fmt.Println("Input:", code)
+		err = repl.StartRepl(code)
+		if err != nil {
+			fmt.Printf("Error: %v\n", err)
 		}
 	}
 }
