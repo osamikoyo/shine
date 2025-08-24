9e01a4a8b99a97ffb1ccce37405a075e8434b7a4
osamikoyo <v.pylayev@mail.ru>
2025-08-25 04:50:55 +0500

    update repl

--- repl.go
+++ repl.go
@@ -19,17 +19,20 @@ func NewRepl() *Repl {
 
 func (r *Repl) StartRepl(code string) error {
 	tokens := Tokenize(code)
-	value, _, err := Read(tokens)
-	if err != nil {
-		return err
+	remaining := tokens
+	for len(remaining) > 0 {
+		value, newRemaining, err := Read(remaining)
+		if err != nil {
+			return fmt.Errorf("read error: %v", err)
+		}
+		remaining = newRemaining
+		fmt.Println("Read result:", Print(value))
+
+		res, err := Eval(value, r.env)
+		if err != nil {
+			return fmt.Errorf("eval error: %v", err)
+		}
+		fmt.Println("Result:", Print(res))
 	}
-
-	res, err := Eval(value, r.env)
-	if err != nil {
-		return err
-	}
-
-	fmt.Println(res)
-
 	return nil
 }
