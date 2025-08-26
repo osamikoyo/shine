0619dbf7cbe665c2ab88bd1368956b26518d1de8
osamikoyo <v.pylayev@mail.ru>
2025-08-26 08:36:12 +0500

    update main.go

--- main.go
+++ main.go
@@ -1,55 +1,90 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"os"
-	"strings"
+	"path/filepath"
 )
 
-func main() {
-	repl := NewRepl()
+var OutputLevel string = "base"
 
-	if len(os.Args) > 2 && os.Args[1] == "-f" {
-		filename := os.Args[2]
-		code, err := os.ReadFile(filename)
+func runWithFile(filename string, repl *Repl) error {
+	code, err := os.ReadFile(filename)
+	if err != nil {
+		fmt.Printf("Error reading file %s: %v\n", filename, err)
+		return err
+	}
+	fmt.Println("Input from file:", string(code))
+
+	err = repl.StartRepl(string(code))
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		return err
+	}
+	return nil
+}
+
+func runForLibs(dirs []string, repl *Repl) error {
+	for _, dir := range dirs {
+		files, err := os.ReadDir(dir)
 		if err != nil {
-			fmt.Printf("Error reading file %s: %v\n", filename, err)
-			os.Exit(1)
+			fmt.Println(err)
+
+			return err
 		}
-		fmt.Println("Input from file:", string(code))
-		err = repl.StartRepl(string(code))
-		if err != nil {
-			fmt.Printf("Error: %v\n", err)
-			os.Exit(1)
+
+		for _, file := range files {
+			code, err := os.ReadFile(filepath.Join(dir, file.Name()))
+			if err != nil {
+				fmt.Println(err)
+				return err
+			}
+
+			if err = repl.StartRepl(string(code)); err != nil {
+				fmt.Println(err)
+				return err
+			}
 		}
-		return
 	}
 
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Println("Welcome to Shine Lisp REPL! Type 'exit' to quit.")
+	return nil
+}
 
-	for {
-		fmt.Print("shine~~> ")
-		code, err := reader.ReadString('\n')
-		if err != nil {
-			fmt.Printf("Error reading input: %v\n", err)
-			break
-		}
+func main() {
+	var (
+		dirs []string
+		file string
+	)
 
-		code = strings.TrimSpace(code)
-		if code == "" {
-			continue
-		}
-		if code == "exit" {
-			fmt.Println("Shine on Your Crazy Diamond!")
-			break
-		}
+	repl := NewRepl()
 
-		fmt.Println("Input:", code)
-		err = repl.StartRepl(code)
-		if err != nil {
-			fmt.Printf("Error: %v\n", err)
+	for i, arg := range os.Args {
+		switch arg {
+		case "--output-level":
+			OutputLevel = os.Args[i+1]
+
+		case "--lib":
+			dirs = append(dirs, os.Args[i+1])
+
+		case "--file":
+			file = os.Args[i+1]
 		}
+	}
+
+	if len(dirs) != 0 {
+		if err := runForLibs(dirs, repl); err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	}
+	if file != "" {
+		if err := runWithFile(file, repl); err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	}
+
+	if err := RouteInput(repl, "shine/history"); err != nil {
+		fmt.Println(err)
 	}
 }
