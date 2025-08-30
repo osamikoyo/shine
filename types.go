15dacecb860de1a9df5c93fe2a8ebd141648b09d
osamikoyo <v.pylayev@mail.ru>
2025-08-30 07:57:33 +0500

    update types

--- types.go
+++ types.go
@@ -1,15 +1,14 @@
 package main
 
 type (
-	Symbol     string
-	Value      interface{}
-	Error      string
-	List       []Value
-	Number     float64
-	String     string
-	Bool       bool
-	Dictionary map[string]Value
-	Func       func(args ...Value) (Value, error)
+	Symbol string
+	Value  interface{}
+	Error  string
+	List   []Value
+	Number float64
+	String string
+	Bool   bool
+	Func   func(args ...Value) (Value, error)
 
 	Lambda struct {
 		params List
